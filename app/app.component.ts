5a8829a697ca389424645d1304ef5adaa77d9391
fayaaz123 <123726554+fayaaz123@users.noreply.github.com>
2025-05-01 11:14:21 +0530

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SubmitClaimComponent } from "./components/submit-claim/submit-claim.component";
 import { AgentDashboardComponent } from "./components/agent-dashboard/agent-dashboard.component";
 import { AdminDashboardComponent } from "./components/admin-dashboard/admin-dashboard.component";
+import { StatusTrackerComponent } from "./components/status-tracker/status-tracker.component";
+import { AdminInsightsComponent } from "./components/admin-insights/admin-insights.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SubmitClaimComponent, AgentDashboardComponent, AdminDashboardComponent],
+  imports: [RouterOutlet, SubmitClaimComponent, AgentDashboardComponent, AdminDashboardComponent, StatusTrackerComponent, RouterLink, AdminInsightsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

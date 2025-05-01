78283851c01806edfbef5b48da19039541ceafd3
fayaaz123 <123726554+fayaaz123@users.noreply.github.com>
2025-05-01 11:14:42 +0530

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http'; // Import provideHttpClient
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient() // Use provideHttpClient instead of HttpClientModule
